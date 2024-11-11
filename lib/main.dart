ed41e1500ef00e7332a6689c9ebd8356c11df63b
chiquixs <nafisachiquita06@gmail.com>
2024-11-11 21:15:32 +0700

    menambahkan teks field dan button

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MainApp extends StatelessWidget {
         home: Scaffold(
       // sebagai kanvan untuk dasar
       appBar: AppBar(
-        title: Text('logo'),
+        title: Text('Login'),
       ),
       body: Container(
         padding: EdgeInsets.only(
@@ -23,15 +23,68 @@ class MainApp extends StatelessWidget {
         height: double.maxFinite,
         width: double.maxFinite,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 24, top: 24d),
+              padding: const EdgeInsets.only(bottom: 24, top: 24),
               child: Text(
-                'sign in',
-                
-                style: TextStyle(fontSize: 20),
+                'sign in to your account',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Text(
+                "enter your email and password to log in",
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+            Text("Email"),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text("password"),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 16),
+              width: double.maxFinite,
+              child: Text(
+                "forgot password?",
+                textAlign: TextAlign.end,
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 40),
+              width: double.maxFinite,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+
+                ),
+                onPressed: () {},
+                child: Text("Login"),
+              ),
+            ),
           ],
         ),
       ),
